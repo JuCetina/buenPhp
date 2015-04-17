a264b69af4c8e1a41dea9db9e882b954cbea09a6
Juliette Cetina <juliethcetina@gmail.com>
2015-04-16 19:55:06 -0500

    Comentarios en Clase RequestUrl.php

--- library/RequestUrl.php
+++ library/RequestUrl.php
@@ -7,15 +7,15 @@ class RequestUrl{
 
 	public function __construct($url)
 	{
-		$this->url = $url;
+		$this->url = $url;  //Guarda la url en la propiedad
 	}
 
 	public function getUrl()
 	{
-		return $this->url;
+		return $this->url; //Obtiene la Url
 	}
 
 	public function getSegments(){
-		return $this->segments = explode('/', $this->getUrl());
+		return $this->segments = explode('/', $this->getUrl()); //Guada en la propiedad segments la url dividida cada /
 	}
 }
