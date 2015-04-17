8500b1322168153873aaac655e836cc9a5fe3bb3
Juliette Cetina <juliethcetina@gmail.com>
2015-04-16 20:10:06 -0500

    Comentarios agregados a Clase Response

--- library/Response.php
+++ library/Response.php
@@ -1,6 +1,6 @@
 <?php
-
-abstract class Response
+//Clase abstracta response, clases que la extiendan estan obligadas a implementar el metodo execute
+abstract class Response 
 {
 	abstract public function execute();
 }
